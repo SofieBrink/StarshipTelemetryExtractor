3bb13b26e906dc4510c74c91244bbb080cf8c957
Sofie <63377159+SofieBrink@users.noreply.github.com>
2024-10-20 08:07:06 +0200

    Move screenreading to seperate class.

--- StarshipTelemetryExtractor/Program.cs
+++ StarshipTelemetryExtractor/Program.cs
@@ -1,10 +1,4 @@
-﻿using Emgu.CV.CvEnum;
-using Emgu.CV;
-using Patagames.Ocr;
-using Patagames.Ocr.Enums;
-using System.Drawing;
-using Emgu.CV.Structure;
-using System.Text.RegularExpressions;
+﻿using Patagames.Ocr;
 
 namespace StarshipTelemetryExtractor
 {
@@ -18,49 +12,13 @@ namespace StarshipTelemetryExtractor
 
             if (!Directory.Exists(InputPath)) { Console.WriteLine($"Inputpath: {InputPath} is invalid"); return; }
             if (!Directory.Exists(OutputPath)) { Console.WriteLine($"Outputpath: {OutputPath} is invalid"); return; }
-            if (Directory.EnumerateFileSystemEntries(OutputPath).Any()) { Console.WriteLine($"Outputpath: {OutputPath} is not empty"); return; }
+
+
+
             var ocr = OcrApi.Create();
             ocr.Init();
 
-            List<(string fileName, int? value)> rawData = new List<(string fileName, int? value)>();
-            List<(string fileName, string? value)> failedData = new List<(string fileName, string? value)>();
-
-            foreach (var entry in Directory.EnumerateFileSystemEntries(InputPath))
-            {
-                var fileName = entry.Split('\\').Last();
-                Mat img = CvInvoke.Imread(entry, ImreadModes.Color);
-                Mat greyImage = new Mat();
-                CvInvoke.CvtColor(img, greyImage, ColorConversion.Bgr2Gray);
-                Mat thresholded = new Mat();
-                CvInvoke.Threshold(greyImage, thresholded, 200, 255, ThresholdType.Binary);
-                Mat dilatedImage = new Mat();
-                Mat kernel = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new Size(1, 1), new Point(-1, -1));
-                CvInvoke.Dilate(thresholded, dilatedImage, kernel, new Point(-1, -1), 2, BorderType.Default, new MCvScalar(1));
-                Mat invertedImage = new Mat();
-                CvInvoke.BitwiseNot(dilatedImage, invertedImage);
-                Bitmap bitmap = invertedImage.ToBitmap();
-
-                var plaintext = ocr.GetTextFromImage(bitmap);
-                if (plaintext != null) plaintext = Regex.Replace(plaintext, @"\s+", "");
-
-                Console.WriteLine($"Processing: {fileName}; Value: {plaintext}");
-
-                if (int.TryParse(plaintext, out var output)) rawData.Add((fileName, output));
-                else { rawData.Add((fileName, null)); failedData.Add((fileName, plaintext)); }
-            }
-
-            List<(string fileName, int? value)> correctedData = new List<(string fileName, int? value)>(rawData);
-
-            if (failedData.Count > 0)
-            {
-                Console.WriteLine($"Failed to get {failedData.Count} value(s), attempting to correct.");
-                CorrectMissingValues(rawData, ref correctedData);
-                foreach (var entry in failedData)
-                {
-                    var correction = correctedData.FirstOrDefault(d => d.fileName == entry.fileName);
-                    Console.WriteLine($"Corrected {entry.fileName} from: \"{entry.value}\" to: {correction.value}");
-                }
-            }
+            ScreenReader.GetTelemetryData(ocr, InputPath, out var rawData, out var correctedData);
 
             using (StreamWriter writer = new StreamWriter(OutputPath + "\\DataOutput.csv"))
             {
@@ -76,41 +34,13 @@ namespace StarshipTelemetryExtractor
                 }
             }
 
-            Console.WriteLine($"Done, took {Math.Round((DateTime.Now - startTime).TotalSeconds, 1)}s");
+            ocr.Dispose();
+
+            Console.WriteLine($"Done, took {Math.Round((DateTime.Now - startTime).TotalSeconds, 1)}s including awaiting input.");
         }
 
 
 
-        static void CorrectMissingValues(List<(string fileName, int? value)> pRawData, ref List<(string fileName, int? value)> rCorrectedData)
-        {
-            int start = -1, end = -1;
-
-            for (int i = 0; i < pRawData.Count; i++)
-            {
-                if (pRawData[i].value == null)
-                {
-                    if (start == -1) start = i;
-                }
-                else if (start != -1)
-                {
-                    end = i;
-                    InterpolateValues(pRawData, ref rCorrectedData, start, end);
-                    start = end = -1;
-                }
-            }
-        }
-
-        static void InterpolateValues(List<(string fileName, int? value)> pRawData, ref List<(string fileName, int? value)> rCorrectedData, int pStart, int pEnd)
-        {
-            int knownValueBefore = pStart >= 0 ? pRawData[pStart - 1].value!.Value : 0; 
-            int knownValueAfter = pRawData[pEnd].value!.Value;
-            int nullCount = pEnd - pStart;
-
-            float step = (knownValueAfter - knownValueBefore) / (nullCount + 1);
-            for (int i = 0; i < nullCount; i++)
-            {
-                rCorrectedData[pStart + i] = (pRawData[pStart + i].fileName, knownValueBefore + (int) (step * (i + 1)));
-            }
-        }
+        
     }
 }
---
+++ StarshipTelemetryExtractor/ScreenReader.cs
@@ -0,0 +1,142 @@
+﻿using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV;
+using Patagames.Ocr;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace StarshipTelemetryExtractor
+{
+    public class ScreenReader
+    {
+        public static void GetTelemetryData(OcrApi pOcr
+                                          , string pPath
+                                          , out List<(string fileName, int? value)> oRawData
+                                          , out List<(string fileName, int? value)> oCorrectedData)
+        {
+            oRawData = new List<(string fileName, int? value)>();
+            oCorrectedData = new List<(string fileName, int? value)>();
+            List<(string fileName, string? value)> failedData = new List<(string fileName, string? value)>();
+
+            if (!Directory.Exists(pPath)) return;
+            foreach (var entry in Directory.EnumerateFileSystemEntries(pPath))
+            {
+                var fileName = entry.Split('\\').Last();
+                Mat img = CvInvoke.Imread(entry, ImreadModes.Color);
+                Mat greyImage = new Mat();
+                CvInvoke.CvtColor(img, greyImage, ColorConversion.Bgr2Gray);
+                Mat thresholded = new Mat();
+                CvInvoke.Threshold(greyImage, thresholded, 200, 255, ThresholdType.Binary);
+                Mat dilatedImage = new Mat();
+                Mat kernel = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new Size(1, 1), new Point(-1, -1));
+                CvInvoke.Dilate(thresholded, dilatedImage, kernel, new Point(-1, -1), 2, BorderType.Default, new MCvScalar(1));
+                Mat invertedImage = new Mat();
+                CvInvoke.BitwiseNot(dilatedImage, invertedImage);
+                Bitmap bitmap = invertedImage.ToBitmap();
+
+                var plaintext = pOcr.GetTextFromImage(bitmap);
+                if (plaintext != null) plaintext = Regex.Replace(plaintext, @"\s+", "");
+
+                Console.WriteLine($"Processing: {fileName}; Value: {plaintext}");
+
+                if (int.TryParse(plaintext, out var output)) oRawData.Add((fileName, output));
+                else { oRawData.Add((fileName, null)); failedData.Add((fileName, plaintext)); }
+            }
+
+            oCorrectedData = new List<(string fileName, int? value)>(oRawData);
+
+            if (failedData.Count > 0)
+            {
+                Console.WriteLine($"Failed to get {failedData.Count} value(s), attempting to correct.");
+                CorrectMissingValues(oRawData, ref oCorrectedData);
+                foreach (var entry in failedData)
+                {
+                    var correction = oCorrectedData.FirstOrDefault(d => d.fileName == entry.fileName);
+                    Console.WriteLine($"Corrected {entry.fileName} from: \"{entry.value}\" to: {correction.value}");
+                }
+            }
+
+            HandleOutliers(oCorrectedData, ref oCorrectedData);
+        }
+
+        static void CorrectMissingValues(List<(string fileName, int? value)> pRawData, ref List<(string fileName, int? value)> rCorrectedData)
+        {
+            int start = -1, end = -1;
+
+            for (int i = 0; i < pRawData.Count; i++)
+            {
+                if (pRawData[i].value == null)
+                {
+                    if (start == -1) start = i;
+                }
+                else if (start != -1)
+                {
+                    end = i;
+                    InterpolateValues(pRawData, ref rCorrectedData, start, end);
+                    start = end = -1;
+                }
+            }
+        }
+
+        static void InterpolateValues(List<(string fileName, int? value)> pRawData, ref List<(string fileName, int? value)> rCorrectedData, int pStart, int pEnd)
+        {
+            int knownValueBefore = pStart >= 0 ? pRawData[pStart - 1].value!.Value : 0;
+            int knownValueAfter = pRawData[pEnd].value!.Value;
+            int nullCount = pEnd - pStart;
+
+            float step = (knownValueAfter - knownValueBefore) / (nullCount + 1);
+            for (int i = 0; i < nullCount; i++)
+            {
+                rCorrectedData[pStart + i] = (pRawData[pStart + i].fileName, knownValueBefore + (int)(step * (i + 1)));
+            }
+        }
+
+        static void HandleOutliers(List<(string fileName, int? value)> pRawData, ref List<(string fileName, int? value)> rCorrectedData)
+        {
+            List<int> lastValues = new List<int>();
+            List<(int oldValue, int index)> outlierIndexes = new List<(int, int)>();
+
+            int allowedErrorMargin = 1500;
+            for (int i = 0; i < pRawData.Count; i++)
+            {
+                if (lastValues.Count == 0) { lastValues.Add((int)pRawData[i].value!); continue; } // if the first value is an outlier, there's nothing we can do.
+                var movingAverage = lastValues.Average();
+
+                if (Math.Abs(movingAverage - (int)pRawData[i].value!) > allowedErrorMargin)
+                {
+                    outlierIndexes.Add(((int)pRawData[i].value!, i));
+                }
+                else
+                {
+                    lastValues.Add((int)pRawData[i].value!);
+                }
+                if (lastValues.Count > 20) lastValues.RemoveAt(0);
+            }
+
+            int start = -1, end = -1;
+            var outlierIndexesList = outlierIndexes.Select(i => i.index);
+
+            for (int i = 0; i < pRawData.Count; i++)
+            {
+                if (outlierIndexesList.Contains(i))
+                {
+                    if (start == -1) start = i;
+                }
+                else if (start != -1)
+                {
+                    end = i;
+                    InterpolateValues(pRawData, ref rCorrectedData, start, end);
+                    start = end = -1;
+                }
+            }
+            if (outlierIndexes.Count > 0)
+            {
+                Console.WriteLine($"Found {outlierIndexes.Count} outlier(s), attempting to correct.");
+                foreach (var i in outlierIndexes)
+                {
+                    Console.WriteLine($"Corrected {pRawData[i.index].fileName} from: \"{i.oldValue}\" to: {rCorrectedData[i.index].value}");
+                }
+            }
+        }
+    }
+}
